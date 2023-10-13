c02675b71a06cbf7355f0cfbd7148982993f7665
Tran Duc Luong <luong8076@gmail.com>
2023-10-13 08:54:19 +0700

    Add javadoc for TreeWord.java

--- src/main/Application/TreeWord.java
+++ src/main/Application/TreeWord.java
@@ -5,17 +5,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * The `TreeWord` class represents a tree-based dictionary.
+ */
 public class TreeWord {
     final private TreeNode root;
 
+    /**
+     * Constructor: Initializes a new tree-based dictionary.
+     */
     public TreeWord() {
         root = new TreeNode();
     }
 
+    /**
+     * Returns the root node of the tree.
+     *
+     * @return The root node.
+     */
     final public TreeNode getRoot() {
         return root;
     }
 
+    /**
+     * Adds a word to the dictionary tree.
+     *
+     * @param w Word to be added.
+     * @return true if added successfully.
+     */
     public boolean addWord(Word w) {
         TreeNode newTree = root;
         String word = w.getWord_target();
@@ -31,28 +48,26 @@ public class TreeWord {
         return true;
     }
 
+    /**
+     * Adds a list of words to the dictionary tree.
+     *
+     * @param listWord List of Word objects to be added.
+     */
     public void addFromList(ArrayList<Word> listWord) {
         for (Word word : listWord) {
             addWord(word);
         }
     }
 
-    public boolean editWord(String currentWord_target, String newWord_target, String newWord_explain) {
-        Word wordEdit = lookup(currentWord_target);
-        if (wordEdit == null) {
-            System.out.println(currentWord_target + " not exist in dictionary");
-            return false;
-        }
-
-        wordEdit.setWord_target(newWord_target);
-        wordEdit.setWord_explain(newWord_explain);
-
-        return delete(currentWord_target) && addWord(wordEdit);
-    }
-
+    /**
+     * Deletes a word from the dictionary.
+     *
+     * @param word The word to delete.
+     * @return true if deleted successfully, false if the word does not exist in the dictionary.
+     */
     public boolean delete(String word) {
         if (lookup(word) == null) {
-            System.out.println(word + " not exist");
+            System.out.println(word + "does not exist in dictionary");
             return false;
         }
 
@@ -60,6 +75,14 @@ public class TreeWord {
         return true;
     }
 
+    /**
+     * Recursively deletes a branch of nodes corresponding to a word in the tree.
+     * Use for method 'delete'.
+     *
+     * @param currentNode  The current node in the traversal.
+     * @param word         The word to delete.
+     * @param currentIndex The current index in the word being processed.
+     */
     public void deleteBranch(TreeNode currentNode, String word, int currentIndex) {
         if (currentIndex == word.length()) {
             currentNode.setCompleteWord(null);
@@ -74,19 +97,33 @@ public class TreeWord {
         }
     }
 
+    /**
+     * Edits a word in the dictionary.
+     *
+     * @param currentWord_target The word to edit.
+     * @param newWord_target     The new word.
+     * @param newWord_explain    The new word explanation.
+     * @return true if the edit was successful, false if the word does not exist in the dictionary.
+     */
+    public boolean editWord(String currentWord_target, String newWord_target, String newWord_explain) {
+        Word wordEdit = lookup(currentWord_target);
+//        if (wordEdit == null) {
+//            System.out.println(currentWord_target + " does not exist in dictionary");
+//            return false;
+//        }
 
+        wordEdit.setWord_target(newWord_target);
+        wordEdit.setWord_explain(newWord_explain);
 
-    private void traversingFromNode(TreeNode node, List<Word> listFound) {
-        if (node.isCompleteWord()) {
-            listFound.add(node.getCompleteWord());
-        }
-
-        if (node.isLastChar()) return;
-        for (Character c : node.getChild().keySet()) {
-            traversingFromNode(node.getChild().get(c), listFound); // BackTracking
-        }
+        return delete(currentWord_target) && addWord(wordEdit);
     }
 
+    /**
+     * Searches for words in the dictionary that start with the specified word.
+     *
+     * @param word The word to start the search from.
+     * @return A list of words that start by 'word'.
+     */
     public List<Word> searchFrom(String word) {
         TreeNode newTree = root;
         List<Word> listWord = new ArrayList<>();
@@ -102,6 +139,30 @@ public class TreeWord {
         return listWord;
     }
 
+    /**
+     * Recursively traverses the tree from a specific node and adds complete words to a list.
+     * Use for method 'searchFrom'.
+     *
+     * @param node      The current node in the traversal.
+     * @param listFound The list to store found words.
+     */
+    private void traversingFromNode(TreeNode node, List<Word> listFound) {
+        if (node.isCompleteWord()) {
+            listFound.add(node.getCompleteWord());
+        }
+
+        if (node.isLastChar()) return;
+        for (Character c : node.getChild().keySet()) {
+            traversingFromNode(node.getChild().get(c), listFound); // BackTracking
+        }
+    }
+
+    /**
+     * Looks up a word in the dictionary.
+     *
+     * @param word The word to look up.
+     * @return The Word object if found, or null if the word is not in the dictionary.
+     */
     public Word lookup(String word) {
         TreeNode newTree = root;
         for (int i = 0; i < word.length(); i++) {
@@ -114,6 +175,9 @@ public class TreeWord {
         return newTree.getCompleteWord();
     }
 
+    /**
+     * Main method for testing the TreeWord class.
+     */
     public static void main(String[] argv) {
         TreeWord test = new TreeWord();
         Scanner scan = new Scanner(System.in);

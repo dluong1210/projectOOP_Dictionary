3380b3c455e30070b3432844c82cfeb6812d9872
TranVuDucHuy <22021111@vnu.edu.vn>
2023-11-29 10:05:17 +0700

    update gameTest

--- src/main/controllers/gameTest.java
+++ src/main/controllers/gameTest.java
@@ -4,9 +4,11 @@ import game.*;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -16,6 +18,7 @@ import javafx.util.Duration;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 import static game.MultipleChoiceGame.option;
 
@@ -24,8 +27,20 @@ public class gameTest implements Initializable {
     private ImageView player;
     @FXML
     private AnchorPane scene;
+    @FXML
+    private Button buttonA;
+    @FXML
+    private Button buttonB;
+    @FXML
+    private Button buttonC;
+    @FXML
+    private Button buttonD;
+//    @FXML
+//    private ;
 
-    private WordChoiceGame game = new WordChoiceGame();     // Su dung game nay by TVDH
+
+    private WordChoiceGame wordGame = new WordChoiceGame();     // Su dung game nay by TVDH
+    private MeaningChoiceGame meaningGame = new MeaningChoiceGame();     // Su dung game nay by TVDH
 
     private ArrayList<ImageView> listBox = new ArrayList<>();
     private final Image boxImage = new Image(getClass().getResourceAsStream("/icon/box.png"));
@@ -59,8 +74,9 @@ public class gameTest implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        game.initGame();            // Chuan bi choi game
-
+        wordGame.initGame();            // Chuan bi choi game
+        meaningGame.initGame();
+        openQuest();
 
         scene.requestFocus();
         for (int i = 1; i < map.length - 1; i++) {
@@ -98,8 +114,10 @@ public class gameTest implements Initializable {
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
+//        Platform.runLater(()->{
+//            if (!isQuest) controllerKey();
+//        });
 
-        if (!isQuest) controllerKey();
     }
 
     public void controllerKey() {
@@ -164,24 +182,67 @@ public class gameTest implements Initializable {
     public void openQuest() {
         // Hiện câu hỏi lên
         //
-        game.getPrepared();  // tao cau hoi va cac lua chon moi
-        String question = game.giveQuestion();
-        String choiceA = game.giveChoice(0);
-        String choiceB = game.giveChoice(1);
-        String choiceC = game.giveChoice(2);
-        String choiceD = game.giveChoice(3);
+        int randomGame = (int) (Math.random() * 2);
+        wordGame.getPrepared();  // tao cau hoi va cac lua chon moi
+        meaningGame.getPrepared();
+        String question;
+        String choiceA;
+        String choiceB;
+        String choiceC;
+        String choiceD;
+        if (randomGame == 0) {
+            question = "Which word has the following meaning? " + wordGame.giveQuestion().substring(14, wordGame.giveQuestion().length());
+            choiceA = wordGame.giveChoice(0);
+            choiceB = wordGame.giveChoice(1);
+            choiceC = wordGame.giveChoice(2);
+            choiceD = wordGame.giveChoice(3);
+        } else {
+            question = "Which is the meaning of the following word? " + meaningGame.giveQuestion().substring(14, meaningGame.giveQuestion().length());
+            choiceA = meaningGame.giveChoice(0);
+            choiceB = meaningGame.giveChoice(1);
+            choiceC = meaningGame.giveChoice(2);
+            choiceD = meaningGame.giveChoice(3);
+        }
+        System.out.println(question);
+        System.out.println(choiceA);
+        System.out.println(choiceB);
+        System.out.println(choiceC);
+        System.out.println(choiceD);
 
         // SetText dong nay
+        buttonA.setText(choiceA);
+        buttonB.setText(choiceB);
+        buttonC.setText(choiceC);
+        buttonD.setText(choiceD);
+
     }
 
     public void getResult() {
         String playerInput = "";   // can 1 method lay input tra ve duoi dang "A" / "B" / "C" / "D"
 
-        boolean result = playerInput.equals(option[game.getCorrectChoice()]);
-
+        boolean result = playerInput.equals(option[wordGame.getCorrectChoice()]);
+        if (result) {
+            increasePoint();
+        } else {
+            decreaseHP();
+        }
         // Co result thi xu li controller tiep
     }
 
+    public void increasePoint() {
+        point += 1;
+        // cap nhat point o giao dien
+        // bat dau vong choi moi
+    }
+
+    public void decreaseHP() {
+        HP -= 1;
+        if (HP == 0) {
+            // endgame
+        } else {
+            // bat dau vong choi moi
+        }
+    }
 
 
     public void changeBox() {
